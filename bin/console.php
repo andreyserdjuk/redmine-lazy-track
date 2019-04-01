b5df1b219929c5bd1718eaeb1934997c8f72e2f7
Andrey Serdjuk <ase6120@gmail.com>
2019-04-01 18:52:39 +0300

    tracking fixes

--- bin/console.php
+++ bin/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use RedmineLogger\Command\ListLastTrackings;
 use RedmineLogger\Command\LogWorkingDays;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -9,6 +10,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new Application('Lazy Redmine', '0.0.1');
 
 $app->add(new LogWorkingDays());
-$app->add(new \RedmineLogger\Command\ListLastTrackings());
+$app->add(new ListLastTrackings());
 
 $app->run(new Symfony\Component\Console\Input\ArgvInput(), new ConsoleOutput());
